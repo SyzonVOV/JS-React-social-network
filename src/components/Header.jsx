d762c88850c70ef53fcf2569ddbfe8a98b188726
SyzonVOV <sizonenko89@gmail.com>
2020-06-27 13:26:01 +0300

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+      <header className='header'>
+        <img src="https://is2-ssl.mzstatic.com/image/thumb/Purple123/v4/42/d5/af/42d5afc2-b3d3-56ad-1650-018544ec1079/AppIcon-1x_U007emarketing-0-7-0-0-85-220.png/1200x630wa.png" alt="Logo"/>
+      </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div className='content'>
+      <div>
+        <img src="https://wallpaperaccess.com/full/1282257.jpg" alt="" width='100%'/>
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New post
+        </div>
+      </div>
+      <div>
+        All posts
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+      <br/>
+      <p>Main content</p>
+    </div>
+  )
+}
+
+export default Profile;
