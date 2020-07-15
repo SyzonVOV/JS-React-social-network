ca1197e8e50e9c80e7ca0905febd53a130cca19d
SyzonVOV <sizonenko89@gmail.com>
2020-07-15 17:18:20 +0300

    modified css

--- src/App.js
+++ src/App.js
@@ -14,19 +14,19 @@ function App(props) {
   console.log(props);
   return (
     <BrowserRouter>
-      <div className='app-wrapper'>
-        <Header/>
-        <NavbarLeft/>
-        <div className='app-wrapper-content'>
-          <Route path='/dialogs'>
-            <Dialogs state={props.state.dialogsPage}/>
-          </Route>
-          <Route path='/profile' render={() => <Profile posts={props.state.profilePage.posts} addPost={props.addPost}/>}/>
-          <Route path='/news' component={News}/>
-          <Route path='/music' component={Music}/>
-          <Route path='/settings' component={Settings}/>
-        </div>
-        <NavbarRigth/>
+      <div>
+        <Header />
+        <NavbarLeft />     
+          <div className='app-wrapper-content'>
+            <Route path='/dialogs'>
+              <Dialogs state={props.state.dialogsPage}/>
+            </Route>
+            <Route path='/profile' render={() => <Profile posts={props.state.profilePage.posts} addPost={props.addPost}/>}/>
+            <Route path='/news' component={News}/>
+            <Route path='/music' component={Music}/>
+            <Route path='/settings' component={Settings}/>
+          </div>
+        <NavbarRigth />
       </div>
 
     </BrowserRouter>
