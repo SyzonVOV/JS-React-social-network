2fd9426c433efbc7b794e2fdd36eb5a74ff742f5
SyzonVOV <sizonenko89@gmail.com>
2020-09-15 19:56:33 +0300

    lesson 54 + componentDidMount()

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,34 +6,34 @@ class Users extends React.Component{
 
   constructor(props) {
     super(props);
-
-    if ( this.props.users.length === 0 ) {
-      let userURL = 'https://upload.wikimedia.org/wikipedia/uk/d/d0/%D0%9A%D0%BD%D1%8F%D0%B7%D1%8C_%D0%92%D0%BE%D0%BB%D0%BE%D0%B4%D0%B8%D0%BC%D0%B8%D1%80_%D0%92%D0%B5%D0%BB%D0%B8%D0%BA%D0%B8%D0%B9.jpg';
-      //axios.get('https://social-network.samuraijs.com/api/1.0/users', { withCredentials: true })
-      axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-        this.props.setUsers(response.data.items.map(item => {
-          return {...item, photoUrl: userURL}
-        }));
-
-      });
-    };
   }
 
+  componentDidMount() {
+    let userURL = 'https://upload.wikimedia.org/wikipedia/uk/d/d0/%D0%9A%D0%BD%D1%8F%D0%B7%D1%8C_%D0%92%D0%BE%D0%BB%D0%BE%D0%B4%D0%B8%D0%BC%D0%B8%D1%80_%D0%92%D0%B5%D0%BB%D0%B8%D0%BA%D0%B8%D0%B9.jpg';
+    //axios.get('https://social-network.samuraijs.com/api/1.0/users', { withCredentials: true })
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+      this.props.setUsers(response.data.items.map(item => {
+        return {...item, photoUrl: userURL}
+      }));
+    });
+  }
 
   render() {
     return <div>
-      { this.props.users.map(user => <div key={ user.id }>
-          <div>
+      { this.props.users.map(user => <div key={ user.id } className={ styles.item }>
+          <div className={ styles.item2 }>
             <div><img src={ user.photos.small != null ? user.photos.small : user.photoUrl} alt="User Photo" className={ styles.userPhoto }/></div>
             <div>{ !user.followed
               ? <button onClick={ () => {this.props.follow(user.id)} }>Follow</button>
               : <button onClick={ () => {this.props.unfollow(user.id)} }>Unfollow</button> }</div>
           </div>
-          <div>
+
+          <div className={ styles.item3 }>
             <div>
               <div>{ user.name }</div>
               <div>{ user.status }</div>
             </div>
+
             <div>
               <div>{ 'user.location.county' }</div>
               <div>{ 'user.location.city' }</div>
